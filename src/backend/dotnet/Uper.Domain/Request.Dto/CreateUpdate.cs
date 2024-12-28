71262a0acf0060a884bef4ac1761e54731572f6b
Arne Halvorsen <arne.halvorsen@gmail.com>
2024-12-28 22:59:57 +0100

    wip on backend, basis code for storing to sql dbs

---
+++ src/backend/dotnet/Uper.Domain/Request.Dto/CreateUpdate.cs
@@ -0,0 +1,19 @@
+using System.Text.Json.Serialization;
+
+namespace Uper.Domain.Request.Dto
+{
+    public class CreateUpdateDto
+    {
+        /// <summary>
+        /// The name of the type (table) the objects belong to.
+        /// </summary>
+        [JsonPropertyName("type")]
+        public required string Type { get; set; }
+
+        /// <summary>
+        /// An array of objects to be created or updated.
+        /// </summary>
+        [JsonPropertyName("objects")]
+        public required List<Dictionary<string, object>> Objects { get; set; }
+    }
+}
